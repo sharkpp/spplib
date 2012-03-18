47ab6f9c574cd40f59df538dbf8e5a037b7cc93d
sharkpp <webmaster@sharkpp.net>
2012-03-18 15:39:48 +0000

    サンク処理のテスト中その２、全てアセンブラで実装するのではなくtemplateで自動でstatic関数を生成できるようにしてみた
    git-svn-id: http://nasserver:888/repos/main/lang/c/spplib@2187 fd21481f-4f5f-b64a-a7cc-914020ce4b36

--- test/thunk_generator/main.cpp
+++ test/thunk_generator/main.cpp
@@ -45,30 +45,69 @@ public:
 class hoge : public Xbyak::CodeGenerator
 {
 	class fuga {
-		void* m_func;
-		size_t m_param_num;
-		size_t m_param_size[32];
+		template<typename T, typename R, typename A1>
+		struct static_invoker {
+			typedef static_invoker<T,R,A1> self_;
+			static void* this_ptr;
+			static R (T::*func_ptr)(A1);
+			static R invoke(A1 a1) {
+				R (T::*p)(A1) = self_::func_ptr;
+				return ((reinterpret_cast<T*>(self_::this_ptr))->*p)(a1);
+			}
+		};
 	public:
-		template<typename T, typename R>                                        fuga(R(T::*func)())         : m_func(*reinterpret_cast<void **>(&func)), m_param_num(0) { TRACE("%s\n", __FUNCSIG__); }
-		template<typename T, typename R, typename A1>                           fuga(R(T::*func)(A1))       : m_func(*reinterpret_cast<void **>(&func)), m_param_num(1) { TRACE("%s\n", __FUNCSIG__); m_param_size[0] = sizeof(A1); }
-		template<typename T, typename R, typename A1, typename A2>              fuga(R(T::*func)(A1,A2))    : m_func(*reinterpret_cast<void **>(&func)), m_param_num(2) { TRACE("%s\n", __FUNCSIG__); m_param_size[0] = sizeof(A1); m_param_size[1] = sizeof(A2); }
-		template<typename T, typename R, typename A1, typename A2, typename A3> fuga(R(T::*func)(A1,A2,A3)) : m_func(*reinterpret_cast<void **>(&func)), m_param_num(3) { TRACE("%s\n", __FUNCSIG__); m_param_size[0] = sizeof(A1); m_param_size[1] = sizeof(A2); m_param_size[2] = sizeof(A3); }
-		void* get() const { return m_func; }
-		size_t countof_param() const { return m_param_num; }
-		size_t sizeof_param(size_t index) const { return m_param_size[index]; }
+		void* thunk_ptr;
+		void* func_ptr;
+		void* func_holder_ptr;
+		void* this_holder_ptr;
+	public:
+
+	//	template<typename T, typename R>
+	//	member_func_thunk(R(T::*func)())
+	//		: m_func(*reinterpret_cast<void **>(&func))
+	//	{ TRACE("%s\n", __FUNCSIG__); }
+
+		template<typename T, typename R, typename A1>
+		fuga(R(T::*func)(A1))
+			: thunk_ptr      (reinterpret_cast<void *>(&static_invoker<T,R,A1>::invoke))
+			, func_ptr       (*reinterpret_cast<void **>(&func))
+			, func_holder_ptr(reinterpret_cast<void *>(&static_invoker<T,R,A1>::func_ptr))
+			, this_holder_ptr(&static_invoker<T,R,A1>::this_ptr)
+		{
+			TRACE("%s\n", __FUNCSIG__);
+		}
+
+	//	template<typename T, typename R, typename A1, typename A2>
+	//	member_func_thunk(R(T::*func)(A1,A2))
+	//		: m_func(*reinterpret_cast<void **>(&func))
+	//	{ TRACE("%s\n", __FUNCSIG__); }
+
+	//	template<typename T, typename R, typename A1, typename A2, typename A3>
+	//	member_func_thunk(R(T::*func)(A1,A2,A3))
+	//		: m_func(*reinterpret_cast<void **>(&func))
+	//	{ TRACE("%s\n", __FUNCSIG__); }
 	};
+	void* m_p;
 public:
 	hoge(){TRACE("%s\n", __FUNCSIG__);}
 	hoge(void* this_, const fuga& f)
 	{
 		TRACE("%s\n", __FUNCSIG__);
-		for(size_t i = 0; i < f.countof_param(); i++) {
-			push(ptr[esp+f.sizeof_param(i)]);
-		}
+//		for(size_t i = 0; i < f.countof_param(); i++) {
+//			push(ptr[esp+f.sizeof_param(i)]);
+//		}
 //		push(ptr[esp+4]);
-        mov(ecx, reinterpret_cast<intptr_t>(this_));
-        call(f.get());
-        ret();
+		//mov(ecx, reinterpret_cast<intptr_t>(this_));
+	//	call(f.get());
+	//	jmp(f.get());
+		//ret();
+
+		// *thunk = this_ptr;
+		mov(dword[f.this_holder_ptr], (DWORD)this_);
+		// *thunk = this_ptr;
+		mov(dword[f.func_holder_ptr], (DWORD)f.func_ptr);
+		// long_jump func
+		jmp(f.thunk_ptr);
 	}
 	~hoge()
 	{
@@ -80,39 +119,66 @@ public:
 	}
 };
 
+template<typename T, typename R, typename A1>
+void* hoge::fuga::static_invoker<T,R,A1>::this_ptr = NULL;
+
+template<typename T, typename R, typename A1>
+R (T::* hoge::fuga::static_invoker<T,R,A1>::func_ptr)(A1) = NULL;
+
+//template<typename T>
+//class foo
+//{
+//public:
+//	static T m_x;
+//	foo(T x){m_x=x;}
+//	static void fuga(T x){TRACE("%d\n", x);}
+//};
+//template<typename T>
+//T foo<T>::m_x = T();
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	SET_TRACE_OUTPUT_DBGOUT();
+
+//	foo<int>::fuga(10);
 
 	test1 v1(987);
 //	spplib::thunk_generator t1(&v1, &test1::run);
 //	(t1.get_code())(123);
 
-	hoge t0v(&v1, &test1::run0v);
-	hoge t0i(&v1, &test1::run0i);
-	hoge t1v(&v1, &test1::run1v);
+//	hoge t0v(&v1, &test1::run0v);
+//	hoge t0i(&v1, &test1::run0i);
+//	hoge t1v(&v1, &test1::run1v);
 	hoge t1i(&v1, &test1::run1i);
-	hoge t1t(&v1, &test1::run1t);
+//	hoge t1t(&v1, &test1::run1t);
 
-	v1.run1i(123);
-	v1.run1i(123);
+	v1.run1v(123);
+	v1.run1v(123);
 
 	TXX z={111,222};
 	v1.run1t(z);
 
-	void(*p0v)()    = (void(*)()   )t0v.get_code();
-	int (*p0i)()    = (int (*)()   )t0i.get_code();
-	void(*p1v)(int) = (void(*)(int))t1v.get_code();
+//	void(*p0v)()    = (void(*)()   )t0v.get_code();
+//	int (*p0i)()    = (int (*)()   )t0i.get_code();
+//	void(*p1v)(int) = (void(*)(int))t1v.get_code();
 	int (*p1i)(int) = (int (*)(int))t1i.get_code();
-	int (*p1t)(TXX) = (int (*)(TXX))t1t.get_code();
+//	int (*p1t)(TXX) = (int (*)(TXX))t1t.get_code();
 
 	int r;
-	    p0v();
-	r = p0i(); TRACE("r=%d\n",r);
-	    p1v(123);
-	r = p1i(123); TRACE("r=%d\n",r);
-	TXX x ={111,222};
-	r = p1t(x); TRACE("r=%d\n",r);
+
+//	    p0v();
+
+//	r = p0i();
+//	TRACE("r=%d\n",r);
+
+//	    p1v(123);
+
+	r = p1i(123);
+	TRACE("r=%d\n",r);
+
+//	TXX x ={111,222};
+//	r = p1t(x);
+//	TRACE("r=%d\n",r);
 
 	return 0;
 }

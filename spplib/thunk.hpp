cf3399eecf9fc4cb5a21ccc6c8de3850659618c9
sharkpp <webmaster@sharkpp.net>
2012-03-20 13:08:30 +0000

    引数が5までのメソッドの変換を実装。 自力でコードを生成するように変更。
    git-svn-id: http://nasserver:888/repos/main/lang/c/spplib@2189 fd21481f-4f5f-b64a-a7cc-914020ce4b36

--- spplib/thunk.hpp
+++ spplib/thunk.hpp
@@ -17,7 +17,7 @@
 /// sharkpp class library namespace
 namespace spplib {
 
-class thunk : public Xbyak::CodeGenerator
+class thunk
 {
 	class func_holder {
 		typedef struct void_ {};
@@ -68,6 +68,28 @@ TRACE("%s\n", __FUNCSIG__);
 				return ((reinterpret_cast<T*>(self_::this_ptr))->*p)(a1,a2,a3);
 			}
 		};
+		template<typename T, typename R, typename A1, typename A2, typename A3, typename A4>
+		struct static_invoker<R(T::*)(A1,A2,A3,A4)> {
+			typedef static_invoker<R(T::*)(A1,A2,A3,A4)> self_;
+			static void* this_ptr;
+			static R (T::*func_ptr)(A1,A2,A3,A4);
+			static R invoke(A1 a1,A2 a2,A3 a3,A4 a4) {
+TRACE("%s\n", __FUNCSIG__);
+				R (T::*p)(A1,A2,A3,A4) = self_::func_ptr;
+				return ((reinterpret_cast<T*>(self_::this_ptr))->*p)(a1,a2,a3,a4);
+			}
+		};
+		template<typename T, typename R, typename A1, typename A2, typename A3, typename A4, typename A5>
+		struct static_invoker<R(T::*)(A1,A2,A3,A4,A5)> {
+			typedef static_invoker<R(T::*)(A1,A2,A3,A4,A5)> self_;
+			static void* this_ptr;
+			static R (T::*func_ptr)(A1,A2,A3,A4,A5);
+			static R invoke(A1 a1,A2 a2,A3 a3,A4 a4,A5 a5) {
+TRACE("%s\n", __FUNCSIG__);
+				R (T::*p)(A1,A2,A3,A4,A5) = self_::func_ptr;
+				return ((reinterpret_cast<T*>(self_::this_ptr))->*p)(a1,a2,a3,a4,a5);
+			}
+		};
 	public:
 		void* thunk_ptr;
 		void* func_ptr;
@@ -110,7 +132,31 @@ TRACE("%s\n", __FUNCSIG__);
 		{
 TRACE("%s\n", __FUNCSIG__);
 		}
+		template<typename T, typename R, typename A1, typename A2, typename A3, typename A4>
+		func_holder(R(T::*func)(A1,A2,A3,A4))
+			: thunk_ptr      (reinterpret_cast<void *>(&static_invoker<R(T::*)(A1,A2,A3,A4)>::invoke))
+			, func_ptr       (*reinterpret_cast<void **>(&func))
+			, func_holder_ptr(reinterpret_cast<void *>(&static_invoker<R(T::*)(A1,A2,A3,A4)>::func_ptr))
+			, this_holder_ptr(&static_invoker<R(T::*)(A1,A2,A3,A4)>::this_ptr)
+		{
+TRACE("%s\n", __FUNCSIG__);
+		}
+		template<typename T, typename R, typename A1, typename A2, typename A3, typename A4, typename A5>
+		func_holder(R(T::*func)(A1,A2,A3,A4,A5))
+			: thunk_ptr      (reinterpret_cast<void *>(&static_invoker<R(T::*)(A1,A2,A3,A4,A5)>::invoke))
+			, func_ptr       (*reinterpret_cast<void **>(&func))
+			, func_holder_ptr(reinterpret_cast<void *>(&static_invoker<R(T::*)(A1,A2,A3,A4,A5)>::func_ptr))
+			, this_holder_ptr(&static_invoker<R(T::*)(A1,A2,A3,A4,A5)>::this_ptr)
+		{
+TRACE("%s\n", __FUNCSIG__);
+		}
 	};
+
+	typedef unsigned char u8;
+	typedef unsigned int  u32;
+	u8* m_code_top;  // フック用コードデータの先頭アドレス
+	u8* m_code_last; // フック用コードデータの先頭アドレス
+	u8* m_code;      // フック用コードデータの先頭アドレス
 
 public:
 
@@ -118,21 +164,81 @@ public:
 
 	thunk(void* this_, const func_holder& f)
 	{
-		// *thunk = this_ptr;
-		mov(dword[f.this_holder_ptr], (DWORD)this_);
-		// *func = funk_ptr;
-		mov(dword[f.func_holder_ptr], (DWORD)f.func_ptr);
-		// long_jump func
-		jmp(f.thunk_ptr);
+#if (defined(WIN32) || defined(_WIN32)) && (!defined(WIN64) || !defined(_WIN64))
+		m_code_top  = m_code = (u8*)VirtualAlloc(NULL, 256, MEM_COMMIT, PAGE_READWRITE);
+		m_code_last = m_code_top + 256;
+		// mov dword ptr [f.this_holder_ptr], this_
+		*m_code++       = 0xC7;
+		*m_code++       = 0x05;
+		*((u32*)m_code) = (u32)f.this_holder_ptr; m_code += sizeof(u32);
+		*((u32*)m_code) = (u32)this_;             m_code += sizeof(u32);
+		// mov dword ptr [f.func_holder_ptr], f.func_ptr
+		*m_code++       = 0xC7;
+		*m_code++       = 0x05;
+		*((u32*)m_code) = (u32)f.func_holder_ptr; m_code += sizeof(u32);
+		*((u32*)m_code) = (u32)f.func_ptr;        m_code += sizeof(u32);
+		// jmp func
+		*m_code++       = 0xE9;
+		*((u32*)m_code) = (u32)f.thunk_ptr - (u32)m_code - 4; m_code += sizeof(u32);
+		// ページ属性を実行のみに変更
+		DWORD op;
+		::VirtualProtect(m_code_top, DWORD(m_code - m_code_top), PAGE_EXECUTE, &op);
+#else
+#error "not impliment!"
+#endif
 	}
 
 	~thunk()
 	{
+		::VirtualFree(m_code_top, 0, MEM_RELEASE);
 	}
 
 	void* get_code()
 	{
-		return (void*)getCode();
+		return (void*)m_code_top;
+	}
+
+	// フック用のコードを挿入
+	void* injection_code(void * func, int x)
+	{
+		return (void*)m_code_top;
+//		u8* code_top = m_code;
+//
+//		DWORD op, op2;
+//		// 関数フックのため一旦属性を書き込みできるようにする
+//		VirtualProtect(func, 5, PAGE_EXECUTE_READWRITE, &op);
+//
+//		// 次コード読み込み関数処理前に関数を割り込ます
+//		u8* code = (u8*)func;
+//
+//		// ※jmp命令があることを前提にしている...
+//		if( 0&&0xE9 != *code )
+//		{
+//			// 改変検知のためページ属性を元に戻す
+//			VirtualProtect(code, 5, op, &op);
+//			return NULL;
+//		}
+//
+//		VirtualProtect(m_code_top, 64, PAGE_EXECUTE_READWRITE, &op2);
+//
+//		memcpy(m_code, code, x);
+//		m_code += x;
+//
+//		u8* jmp_addr  = (u8*)((u32)code + x);
+//	//	u8* jmp_addr  = (u8*)((u32)code + *((u32*)(code+1)) + 5);
+//		*code++       = 0xE9; // jmp
+//		*((u32*)code) = (u32)m_code_top - (u32)code - 4;
+//
+//		// 改変検知のためページ属性を元に戻す
+//		VirtualProtect(code, 5, op, &op);
+//
+//		*m_code++       = 0xE9; // jmp
+//		*((u32*)m_code) = (u32)jmp_addr - (u32)m_code - 4; m_code += sizeof(u32);
+//
+//		// ページ属性を実行のみに変更
+//		::VirtualProtect(m_code_top, DWORD(m_code - m_code_top), PAGE_EXECUTE, &op2);
+//
+//		return code_top;
 	}
 };
 
@@ -148,100 +254,11 @@ template<typename T, typename R, typename A1, typename A2> R (T::* thunk::func_h
 template<typename T, typename R, typename A1, typename A2, typename A3> void*   thunk::func_holder::static_invoker<R(T::*)(A1,A2,A3)>::this_ptr            = NULL;
 template<typename T, typename R, typename A1, typename A2, typename A3> R (T::* thunk::func_holder::static_invoker<R(T::*)(A1,A2,A3)>::func_ptr)(A1,A2,A3) = NULL;
 
-/*
-// 
-template<typename T>
-class thunk_generator
-{
-	typedef unsigned char u8;
-	typedef unsigned int  u32;
-	u8* m_code_top;  // フック用コードデータの先頭アドレス
-	u8* m_code_last; // フック用コードデータの先頭アドレス
-	u8* m_code;      // フック用コードデータの先頭アドレス
-public:
-	static T * thunk;
-public:
+template<typename T, typename R, typename A1, typename A2, typename A3, typename A4> void*   thunk::func_holder::static_invoker<R(T::*)(A1,A2,A3,A4)>::this_ptr               = NULL;
+template<typename T, typename R, typename A1, typename A2, typename A3, typename A4> R (T::* thunk::func_holder::static_invoker<R(T::*)(A1,A2,A3,A4)>::func_ptr)(A1,A2,A3,A4) = NULL;
 
-	// コンストラクタ
-	thunk_generator(void * this_ptr, void * func, size_t code_len = 256)
-	{
-#if (defined(WIN32) || defined(_WIN32)) && (!defined(WIN64) || !defined(_WIN64))
-		m_code_top  = m_code = (LPBYTE)VirtualAlloc(NULL, code_len, MEM_COMMIT, PAGE_READWRITE);
-		m_code_last = m_code_top + code_len;
-		// mov DWORD PTR [&thunk], this_ptr
-		*m_code++       = 0xC7;
-		*m_code++       = 0x05;
-		*((u32*)m_code) = (u32)&thunk;   m_code += sizeof(u32);
-		*((u32*)m_code) = (u32)this_ptr; m_code += sizeof(u32);
-		// jmp func
-		*m_code++       = 0xE9;
-		*((u32*)m_code) = (u32)func - (u32)m_code - 4; m_code += sizeof(u32);
-		// ページ属性を実行のみに変更
-		DWORD op;
-		::VirtualProtect(m_code_top, DWORD(m_code - m_code_top), PAGE_EXECUTE, &op);
-#else
-#error "not impliment!"
-#endif
-	}
-
-	// デストラクタ
-	~thunk_generator()
-	{
-		::VirtualFree(m_code_top, 0, MEM_RELEASE);
-	}
-
-	// フック用のコードを取得
-	void* get_code()
-	{
-		return m_code_top;
-	}
-
-	// フック用のコードを挿入
-	void* injection_code(void * func, int x)
-	{
-		u8* code_top = m_code;
-
-		DWORD op, op2;
-		// 関数フックのため一旦属性を書き込みできるようにする
-		VirtualProtect(func, 5, PAGE_EXECUTE_READWRITE, &op);
-
-		// 次コード読み込み関数処理前に関数を割り込ます
-		u8* code = (u8*)func;
-
-		// ※jmp命令があることを前提にしている...
-		if( 0&&0xE9 != *code )
-		{
-			// 改変検知のためページ属性を元に戻す
-			VirtualProtect(code, 5, op, &op);
-			return NULL;
-		}
-
-		VirtualProtect(m_code_top, 64, PAGE_EXECUTE_READWRITE, &op2);
-
-		memcpy(m_code, code, x);
-		m_code += x;
-
-		u8* jmp_addr  = (u8*)((u32)code + x);
-	//	u8* jmp_addr  = (u8*)((u32)code + *((u32*)(code+1)) + 5);
-		*code++       = 0xE9; // jmp
-		*((u32*)code) = (u32)m_code_top - (u32)code - 4;
-
-		// 改変検知のためページ属性を元に戻す
-		VirtualProtect(code, 5, op, &op);
-
-		*m_code++       = 0xE9; // jmp
-		*((u32*)m_code) = (u32)jmp_addr - (u32)m_code - 4; m_code += sizeof(u32);
-
-		// ページ属性を実行のみに変更
-		::VirtualProtect(m_code_top, DWORD(m_code - m_code_top), PAGE_EXECUTE, &op2);
-
-		return code_top;
-	}
-};
-
-template<typename T>
-T * thunk_generator<T>::thunk = NULL;
-*/
+template<typename T, typename R, typename A1, typename A2, typename A3, typename A4, typename A5> void*   thunk::func_holder::static_invoker<R(T::*)(A1,A2,A3,A4,A5)>::this_ptr                  = NULL;
+template<typename T, typename R, typename A1, typename A2, typename A3, typename A4, typename A5> R (T::* thunk::func_holder::static_invoker<R(T::*)(A1,A2,A3,A4,A5)>::func_ptr)(A1,A2,A3,A4,A5) = NULL;
 
 } // namespace spplib
 
